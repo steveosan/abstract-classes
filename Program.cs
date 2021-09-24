52c141346f453c4d55134a242cc94abee44a7aa8
steveosan <70219345+steveosan@users.noreply.github.com>
2021-09-23 21:55:28 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace In_Class_Abstract_Class
 {
@@ -7,22 +7,16 @@ namespace In_Class_Abstract_Class
         static void Main(string[] args)
         {
 
-            Vehicle vehicle = new Vehicle();
 
-            vehicle.Start();
-            vehicle.Drive();
+            Ford ford = new Ford();
+            ford.Start();
+            
+            Vehicle f150 = new Ford();
+            f150.ShowColor();
 
-            //I'm not sure whats going on Mark - I've been trying to fix my inntellisense. It is not displaying any 
-            //errors and you click pretty fast =)  
-            //I'm going to rewatch this class a few times. This is going to take me a few watches to sink in... 
-             
+            Vehicle camery = new Toyota();
 
-
-
-            // Vehicle myFord = new Ford():
-            // myFord.Make = "Ford";
-
-            // myFord.Start();
+            camery.ShowCarFax();
 
         }
     }
