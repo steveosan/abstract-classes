dead487d41b448c666dca0d8a24d775d0c54f429
steveosan <70219345+steveosan@users.noreply.github.com>
2021-09-23 21:55:48 -0500

    Update Ford.cs

--- Ford.cs
+++ Ford.cs
@@ -1,12 +1,26 @@
 namespace In_Class_Abstract_Class
 {
-    public abstract class Ford : Vehicle
+    public class Ford : Vehicle
     {
-        public string Color {get; set;}
+        public override string Color {get; set;}
 
         public override void Drive()
         {
+            System.Console.WriteLine("you are driving a ford");
+        }
+        
+
+        public override void Start()
+        {
             System.Console.WriteLine("The Ford is running");
+        }
+
+        public override void ShowColor()
+        {
+            Make = "ford";
+            Model = "Focus";
+            Color = "blue";
+            System.Console.WriteLine($"The color is {Color}");
         }
 
 
